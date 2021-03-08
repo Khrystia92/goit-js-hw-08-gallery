50b2195c32805060bf68f9fa6a409673ce971dbe
Kristina Kasianenko <65248786+Khrystia92@users.noreply.github.com>
2021-03-08 18:29:31 +0200

    add Modale

--- gallery.js
+++ gallery.js
@@ -1,12 +1,12 @@
-import galleryItems from "./gallery-items";
+import galleryItems from "./gallery-items.js";
 const references = {
  gallery: document.querySelector('.js-gallery'),
  lightbox: document.querySelector('.lightbox'),
- overlay: document.querySelector('.lightbox__overlay'),
- image: document.querySelector('.lightbox__image'),
- button: document.querySelector('lightbox__button'),
+ lightboxOverlay: document.querySelector('.lightbox__overlay'),
+ lightboxImage: document.querySelector('.lightbox__image'),
+ closeButton: document.querySelector('lightbox__button'),
 };
-
+let activeIn
 const createGalleryMarkup = galleryItems.map(({preview, original, description}, index) => setGalleryItems(preview, original, description, index)).join('');
 
 function setGalleryItems(preview, original, description) {
@@ -19,4 +19,31 @@ function setGalleryItems(preview, original, description) {
   alt = "${description}"/>
   </a>
   </li>`
+}
+references.gallery.insertAdjacentHTML('beforeend', createGalleryMarkup)
+
+function openIMG(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  references.lightboxImage.src = event.target.dataset.source;
+  references.lightboxImage.alt = event.target.alt;
+  openModal()
+  activeIn = Number(event.target.dataset.index);
+}
+function closeModal() {
+  references.lightbox.classList.remove('is-open')
+  window.removeEventListener('keydown', onkeypress);
+  references.lightboxImage.src = '';
+  references.lightboxImage.alt = '';
+}
+references.gallery.addEventListener('click', openIMG);
+references.closeButton.addEventListener('click', closeModal);
+references.lightboxOverlay.addEventListener('click', lightboxOverlayClose)
+
+function lightboxOverlayClose(event) {
+  if (event.currentTarget === event.target) {
+    closeModal();
+  }
 }
