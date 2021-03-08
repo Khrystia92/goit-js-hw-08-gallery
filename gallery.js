cbe78f15f3164d681450c3d22eb862c24f4b003e
Kristina Kasianenko <65248786+Khrystia92@users.noreply.github.com>
2021-03-08 18:06:48 +0200

    add Markup

--- gallery.js
+++ gallery.js
@@ -1,4 +1,22 @@
 import galleryItems from "./gallery-items";
 const references = {
-  gallery: document.querySelector('.js-gallery'),
+ gallery: document.querySelector('.js-gallery'),
+ lightbox: document.querySelector('.lightbox'),
+ overlay: document.querySelector('.lightbox__overlay'),
+ image: document.querySelector('.lightbox__image'),
+ button: document.querySelector('lightbox__button'),
+};
+
+const createGalleryMarkup = galleryItems.map(({preview, original, description}, index) => setGalleryItems(preview, original, description, index)).join('');
+
+function setGalleryItems(preview, original, description) {
+  return `<li class = "gallery__item">
+  <a class = "gallery__link" href = "${original}"> 
+  <img clas = "gallery__image"
+  src = "${preview}"
+  data-source = "${original}"
+  data-index = "${index}"
+  alt = "${description}"/>
+  </a>
+  </li>`
 }
